9a3490b4bc9b97a368029b9c29e2cce52c8afb6b
Brandon Mikel <Brandon.Mikel@gmail.com>
2021-01-03 14:49:57 +1300

    Song titles more random

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ SCREAM_DIR = AUDIO_DIR + "screams/";
 AMBIANCE_COUNT = 20;
 AMBIANCE_DIR = AUDIO_DIR + "soundtracks/";
 
-screams = [...Array(SCREAM_COUNT).keys()].map(i => new Audio(SCREAM_DIR + (i + 1) + ".wav"));
-ambiances = [...Array(AMBIANCE_COUNT).keys()].map(i => new Audio(AMBIANCE_DIR + (i + 1) + ".mp3"));
+var screams = [...Array(SCREAM_COUNT).keys()].map(i => new Audio(SCREAM_DIR + (i + 1) + ".wav"));
+var ambiances = [...Array(AMBIANCE_COUNT).keys()].map(i => new Audio(AMBIANCE_DIR + (i + 1) + ".mp3"));
 
-current_scream_id = -1;
-current_ambiance_id = -1;
+let current_scream_id = -1;
+let current_ambiance_id = -1;
 
 var prefixes = ["Deadly","Death","Scarlet","Forbidden","Feral","Mourning","Destined","Hallowed","Demon","Death","Fallen","Demon","Slaves","Raven","Phantom", "Blood", "Satan", "Hell", "Thirsty", "Starving", "Corpse"];
 var suffixes = ["Embrace","Craving","Mistress","Betrayal","Secret","Love","Shadow","Hunger","Wine","Gravy","Sins","Moon","Spiders","Eternal","Sorrow","Light","Lament", "Moonlight", "Dungeons", "Torturer", "Yearnings", "Decay", "Silence"];
 var mixins = ["And", "Of The", "Of", "In", "With Endless", "Under The", "Betwixt The"];
+
+let current_prefix_id = -1;
+let current_suffix_id = -1;
+let current_mixin_id = -1;
 
 function getRandomInt(max, lastValue = -1) {
   var randomInt = Math.floor(Math.random() * Math.floor(max));
@@ -25,14 +29,17 @@ function getRandomInt(max, lastValue = -1) {
 }
 
 function getRandomTitle() {
-  var prefix = prefixes[getRandomInt(prefixes.length)] + " ";
-  var suffix = suffixes[getRandomInt(suffixes.length)];
+  current_prefix_id = getRandomInt(prefixes.length, current_prefix_id);
+  current_suffix_id = getRandomInt(suffixes.length, current_suffix_id);
+
+  var prefix = prefixes[current_prefix_id] + " ";
+  var suffix = suffixes[current_suffix_id];
 
   var mixin = "";
 
-  var hasMixin = Math.random() > 0.7;
-  if (hasMixin) {
-    mixin = mixins[getRandomInt(mixins.length)] + " ";
+  if (Math.random() < 0.3) {
+    current_mixin_id = getRandomInt(mixins.length, current_mixin_id);
+    mixin = mixins[current_mixin_id] + " ";
   }
 
   return prefix + mixin + suffix;
