86000f4bbe2b851fee8718ac96c968a964256124
Brandon Mikel <Brandon.Mikel@gmail.com>
2021-01-07 21:03:37 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-AUDIO_DIR = "assets/sounds/";
+const DEBUG_MODE = true;
+const AUDIO_DIR = "assets/sounds/";
 
-SCREAM_COUNT = 24;
-SCREAM_DIR = AUDIO_DIR + "screams/";
+const SCREAM_COUNT = 24;
+const SCREAM_DIR = AUDIO_DIR + "screams/";
 
-AMBIANCE_COUNT = 20;
-AMBIANCE_DIR = AUDIO_DIR + "soundtracks/";
+const AMBIANCE_COUNT = 20;
+const AMBIANCE_DIR = AUDIO_DIR + "soundtracks/";
 
 var screams = [...Array(SCREAM_COUNT).keys()].map(i => new Audio(SCREAM_DIR + (i + 1) + ".wav"));
 var ambiances = shuffle([...Array(AMBIANCE_COUNT).keys()].map(i => new Audio(AMBIANCE_DIR + (i + 1) + ".mp3")));
 
+const prefixes = ["Deadly","Death","Scarlet","Forbidden","Feral","Mourning","Destined","Hallowed","Demon","Death","Fallen","Demon","Slaves","Raven","Phantom", "Blood", "Satan", "Hell", "Thirsty", "Starving", "Corpse"];
+const suffixes = ["Embrace","Craving","Mistress","Betrayal","Secret","Love","Shadow","Hunger","Wine","Gravy","Sins","Moon","Spiders","Eternal","Sorrow","Light","Lament", "Moonlight", "Dungeons", "Torturer", "Yearnings", "Decay", "Silence"];
+const mixins = ["And", "Of The", "Of", "In", "With Endless", "Under The", "Betwixt The"];
+
 let current_scream_id = -1;
 let current_ambiance_id = -1;
-
-var prefixes = ["Deadly","Death","Scarlet","Forbidden","Feral","Mourning","Destined","Hallowed","Demon","Death","Fallen","Demon","Slaves","Raven","Phantom", "Blood", "Satan", "Hell", "Thirsty", "Starving", "Corpse"];
-var suffixes = ["Embrace","Craving","Mistress","Betrayal","Secret","Love","Shadow","Hunger","Wine","Gravy","Sins","Moon","Spiders","Eternal","Sorrow","Light","Lament", "Moonlight", "Dungeons", "Torturer", "Yearnings", "Decay", "Silence"];
-var mixins = ["And", "Of The", "Of", "In", "With Endless", "Under The", "Betwixt The"];
 
 let current_prefix_id = -1;
 let current_suffix_id = -1;
 let current_mixin_id = -1;
+
+const MIXIN_CHANCE = 0.3;
+
+const MAX_AMBIANCE_VOLUME = 0.7;
+const CROSSFADE_STEPS = 4;
+const CROSSFADE_DURATION = 1;
+
+function debug(message) {
+  if (DEBUG_MODE === true)
+  {
+    console.log(message);
+  }
+}
 
 function getRandomInt(max, lastValue = -1) {
   var randomInt = Math.floor(Math.random() * Math.floor(max));
@@ -37,7 +51,7 @@ function getRandomTitle() {
 
   var mixin = "";
 
-  if (Math.random() < 0.3) {
+  if (Math.random() < MIXIN_CHANCE) {
     current_mixin_id = getRandomInt(mixins.length, current_mixin_id);
     mixin = mixins[current_mixin_id] + " ";
   }
@@ -46,17 +60,38 @@ function getRandomTitle() {
 }
 
 function stopClip(clip) {
-  if (clip)
+  if (clip && !clip.paused)
   {
     clip.pause();
-    clip.currentTime = 0
+    clip.currentTime = 0;
+    debug("Stopping audio:");
+    audioInfo(clip, false);
   }
 };
 
 function silencio() {
   stopClip(screams[current_scream_id]);
+
   stopClip(ambiances[current_ambiance_id]);
+  stopClip(buffer_track);
+
   document.getElementById('track-title').innerHTML = "None";
+}
+
+function updateGains ( audio, volume ) {
+  audio.volume = volume;
+  debug(`Audio volume now ${volume}`);
+}
+
+function audioInfo (audio, duration = true) {
+  let fileBits = audio.src.split('/');
+  let name = fileBits[fileBits.length - 1];
+  let type = fileBits[fileBits.length - 2];
+  debug(`\tAudio file: ${type}/${name}`);
+  if (duration)
+  {
+    debug(`\tDuration: ${audio.duration} seconds`);
+  }
 }
 
 function playScreamClip() {
@@ -64,7 +99,16 @@ function playScreamClip() {
 
   current_scream_id = getRandomInt(SCREAM_COUNT, current_scream_id);
 
-  screams[current_scream_id].play();
+  var scream = screams[current_scream_id];
+  scream.play();
+
+  debug(`Scream playing:`);
+  audioInfo(scream, false);
+
+  scream.addEventListener('ended', function(){
+    debug(`Scream finished:`);
+    audioInfo(this);
+  });
 };
 
 function nextAmbiance() {
@@ -92,15 +136,53 @@ function changeTitle() {
 
 function playAmbiance(audio) {
   audio.addEventListener('timeupdate', function(){
-      var buffer = .75
-      if(this.currentTime > this.duration - buffer){
-          this.currentTime = 0.3
-          this.play()
-      }
+    var buffer = .5
+    if(this.currentTime > this.duration - buffer){
+      var buffer_track = this.cloneNode();
+      playAmbiance(buffer_track);
+      audioVolumeOut(this);
+      stopClip(this);
+      ambiances[current_ambiance_id] = buffer_track;
+    }
   });
 
-  audio.volume = 0.7;
-  audio.loop = true;
+  updateGains(audio, MAX_AMBIANCE_VOLUME);
 
   audio.play();
+  debug(`Playing new ambiance:`);
+  audioInfo(audio);
 }
+
+function audioVolumeIn(q){
+  if(q.volume){
+     var InT = 0;
+     var setVolume = MAX_AMBIANCE_VOLUME; // Target volume level for new song
+     var speed = 0.05; // Rate of increase
+     updateGains(q, InT);
+     var eAudio = setInterval(function(){
+         InT += speed;
+         updateGains(q, InT.toFixed(1));
+         if(InT.toFixed(1) >= setVolume){
+            clearInterval(eAudio);
+            //alert('clearInterval eAudio'+ InT.toFixed(1));
+         };
+     },50);
+  };
+};
+
+function audioVolumeOut(q){
+  if(q.volume){
+     var InT = q.volume;
+     var setVolume = 0;  // Target volume level for old song
+     var speed = 0.05;  // Rate of volume decrease
+     updateGains(q, InT);
+     var fAudio = setInterval(function(){
+         InT -= speed;
+         updateGains(q, InT.toFixed(1));
+         if(InT.toFixed(1) <= setVolume){
+            clearInterval(fAudio);
+            //alert('clearInterval fAudio'+ InT.toFixed(1));
+         };
+     },50);
+  };
+};
